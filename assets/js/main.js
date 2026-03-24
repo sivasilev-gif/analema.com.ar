922cf33698deb9c7dd0b1e06cff7e7085daa9930
sivasilev-gif <sivasilev@gmail.com>
2026-03-24 00:48:17 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.querySelector(".menu-toggle");
   const menu = document.querySelector("nav ul");
 
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("open");
-  });
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("open");
+    });
+  }
 });
